b0101717c99833457b359767a81c4455d32e5114
Vishwa Prakash <kenshelby097@gmail.com>
2026-01-22 22:54:13 +0530

    added style, fav-icon and res-card

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo-500x281.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                    <li>
+                        <img className="cart-logo" src="https://cdn-icons-png.flaticon.com/512/6000/6000305.png"/>
+                    </li>
+                </ul>                
+            </div>
+        </div>
+    )
+}
 
-// const heading = React.createElement("h1", {}, "Text inside h1");    
-const parent = React.createElement("div", { id: "parent" }, 
-    [
-      "this text is for the parent div", 
-      React.createElement("div", {className: "child1", key: "child1"}, [
-            "text of child1 div", 
-            React.createElement("h1", {key: "child1-h1"}, "Text inside h1"), 
-            React.createElement("h2", {key: "child1-h2"}, "Text inside h2")]
-        ), 
-      React.createElement("div", {className: "child2", key: "child2"}, [
-            "text of child2 div", 
-            React.createElement("h1", {key: "child2-h1"}, "Text inside h1"), 
-            React.createElement("h2", {key: "child2-h2"}, "Text inside h2")]
-        )
-    ]
-)
+const RestaurantCard = () => {
+    return(
+        <div className="res-card">
+            <img className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"></img>
+            <h3 className="res-name">Meghana Foods</h3>
+            <h4 className="res-rating">4.5</h4>
+        </div>
+    )
+}
+const Body = () =>{
+    return(
+        <div className="body">
+            <div className="search">
+                <input className="input-box"></input>
+                <button className="search-button">search</button>
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+const Footer = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return(
+        <div>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout/>);
