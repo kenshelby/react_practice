cd6c1a9416cab828baf980854896e25600a5b5fc
Vishwa Prakash <kenshelby097@gmail.com>
2026-02-07 02:06:57 +0530

    added locality, costForTwo var

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,14 +3,16 @@ import { resImg } from "../utils/config";
 
 const RestaurantCard = (props) => {
 
-    const { id, name, cloudinaryImageId, costForTwo, cuisines, avgRating } = props.resData.card.card.info;
+    const { name, cloudinaryImageId, costForTwo, cuisines, avgRating, locality } = props.resData.card.card.info;
     return(
         <div className="res-card">
             <img className="res-img" src={resImg + cloudinaryImageId}></img>
             <h4 className="res-name">{name}</h4>
+            <span>{locality}</span>
             <div className="res-rating">
                 <RatingIcon size={14} />
-                <span className="rating-value">{avgRating}</span>
+                <span className="rating-value" style={{padding: "4px"}}>{avgRating}</span>
+                <span className="cost" style={{padding: "20px"}}>{costForTwo}</span>
             </div>
             <div className="cuisine">{cuisines.join(", ")}</div>
         </div>
