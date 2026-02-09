5dda72e1623e5df7d0ed4186f820afe955014cac
Vishwa Prakash <kenshelby097@gmail.com>
2026-02-09 23:55:28 +0530

    added route to app.js

--- src/app.js
+++ src/app.js
@@ -3,16 +3,24 @@ import ReactDOM from "react-dom/client";
 import Header from "../components/Header.js";
 import Body from "../components/Body.js";
 import Footer from "../components/Footer.js";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
     return(
         <div>
             <Header/>
-            <Body/>
+            <Outlet/>
             <Footer/>
         </div>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(
+            <Router>
+                <Routes>
+                    <Route path="/" element={<AppLayout/>}></Route>
+                    <Route index element={<Body/>}></Route>
+                </Routes>
+            </Router>
+);
